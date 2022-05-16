4c246fe2c13a10e8bba6f5838facbc5132cb17f9
Bond-Anna <anna93bondarenko@gmail.com>
2022-05-16 15:04:30 +0300

    fixing directions

--- src/containers/Private/Weather/index.tsx
+++ src/containers/Private/Weather/index.tsx
@@ -3,12 +3,15 @@ import { useState } from 'react'
 import { observer } from 'mobx-react-lite'
 import Autocomplete from 'react-google-autocomplete'
 import cn from 'classnames'
+import { useTranslation } from 'react-i18next'
+import 'utils/i18n'
 import citys from './store/citys'
 import Card from './Card/Card'
 import LangDrop from './LangDrop'
 import styles from './styles.module.scss'
 
 const Weather: FC = observer(() => {
+  const { t } = useTranslation()
   const [autocomplete, setAutocomplete] = useState<string>('')
 
   const getWeather = (name: string): void => {
@@ -37,7 +40,7 @@ const Weather: FC = observer(() => {
       </div>
       <div className={styles.input}>
         <Autocomplete
-          placeholder="City.."
+          placeholder={`${t('placeholder')}..`}
           // @ts-ignore
           value={autocomplete}
           apiKey="AIzaSyA9bslaj5Bl5nLuQQXe8rr_PkhDvvZqzMs"
